68fad4949bf6cee42372b735b735238a4cc6e450
Hugo <hugohu0224@gmail.com>
2024-05-24 13:47:43 +0800

    Add a web page to allow users to search sport events

--- internal/webserver/static/app.js
+++ internal/webserver/static/app.js
@@ -1,25 +1,54 @@
+function formatDate(timestamp) {
+    const d = new Date(timestamp * 1000);
+    let year = d.getFullYear();
+    let month = (d.getMonth() + 1).toString().padStart(2, '0');
+    let day = d.getDate().toString().padStart(2, '0');
+    let hour = d.getHours().toString().padStart(2, '0');
+    let minute = d.getMinutes().toString().padStart(2, '0');
+    let second = d.getSeconds().toString().padStart(2, '0');
+    return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+}
+
 function searchEvents() {
     const leagueName = document.getElementById('leagueName').value;
     const sportType = document.getElementById('sportType').value;
     const startDate = document.getElementById('startDate').value;
     const endDate = document.getElementById('endDate').value;
 
-    const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            LeagueName: leagueName,
-            SportType: sportType,
-            StartDate: startDate,
-            EndDate: endDate
-        })
-    };
+    const queryParams = new URLSearchParams({
+        leagueName: leagueName,
+        sportType: sportType,
+        startDate: startDate,
+        endDate: endDate
+    }).toString();
 
-    fetch('http://172.23.199.161:8080/v1/event/', requestOptions)
+    fetch(`http://localhost:8080/v1/event/?${queryParams}`)
         .then(response => response.json())
         .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = JSON.stringify(data, null, 2);
+            const events = data.data.events;
+            const tableBody = document.getElementById('eventTable').getElementsByTagName('tbody')[0];
+            tableBody.innerHTML = '';
+
+            events.forEach(event => {
+                let formattedTimestamp = formatDate(event.timestamp); // 格式化时间戳
+                let row = `<tr>
+                    <td>${event.leagueName}</td>
+                    <td>${event.date}</td>
+                    <td>${formattedTimestamp}</td>
+                    <td>${event.raceTime}</td>
+                    <td>${event.homeName}</td>
+                    <td>${event.awayName}</td>
+                    <td>${event.score}</td>
+                    <td>${event.homeOdds}</td>
+                    <td>${event.awayOdds}</td>
+                </tr>`;
+                tableBody.innerHTML += row;
+            });
         })
         .catch(error => console.error('Error:', error));
 }
+
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    searchEvents();
+});
